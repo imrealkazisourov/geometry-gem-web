7eee3fe8ddad3324a706481fcfe22dcc647e4115
Kazi Sourov <iamkazisourov@gmail.com>
2024-06-19 09:07:06 +0600

    changed basic logics on js

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -3,13 +3,12 @@ function triangleCalculateArea() {
     const triangleBaseInput = document.getElementById('triangle-base');
     const triangleBaseText = triangleBaseInput.value;
     //converting string into float number. 
-    const base = parseFloat(triangleBaseText);
+    const triangleBase = parseFloat(triangleBaseText);
     console.log(typeof base);
 
     //getting triangle height value.
     const triangleHeightInput = document.getElementById('triangle-height');
     const triangleHeightText = triangleHeightInput.value;
 
-    const height = parseFloat(triangleHeightText);
-    console.log(typeof height);
+    const triangleHeight = parseFloat(triangleHeightText)
 }
